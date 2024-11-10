91f8f5f8c17104260a39075fe1daae62bbf77ade
nealgoogs <ngeoghegan@my.ccsu.edu>
2024-11-09 23:38:29 -0500

    portfolio

--- index.js
+++
@@ -1,9 +0,0 @@
-function animateText() {
-    const text = document.getElementById("welcome-text");
-    text.classList.add("animate");
-  
-    setTimeout(() => {
-      text.classList.remove("animate");
-    }, 1000);
-  }
-  
